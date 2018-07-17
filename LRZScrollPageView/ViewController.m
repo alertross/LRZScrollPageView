4d393ccacf4ad86c74f5ce5483b91be18b2aa146
LQ <15839956590@163.com>
2018-07-17 10:27:43 +0800

    project init

---
+++ LRZScrollPageView/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LRZScrollPageView
+//
+//  Created by 刘强 on 2018/7/17.
+//  Copyright © 2018年 LightReason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
